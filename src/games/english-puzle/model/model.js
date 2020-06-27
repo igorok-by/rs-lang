56e1f6c5ec13796a55479f7fcbe3a7de26022465
rgovin <rgovin@allvision.by>
2020-06-27 17:47:29 +0300

    feat: menu buttons via model

--- src/games/english-puzle/model/model.js
+++ src/games/english-puzle/model/model.js
@@ -5,10 +5,42 @@ class EnglishPuzzleModel extends Model {
     super();
     this.words = [];
     this.count = 0;
+    this.settings = {
+      autoPronunciation: true,
+      translate: true,
+      pronunciation: true,
+      picture: false,
+    };
+
+    this.levels = [];
+    this.pages = [];
   }
 
   bindDisplayWords(cb) {
     this.displayWords = cb;
+  }
+
+  bindDisplaySettings(cb) {
+    this.displaySettings = cb;
+  }
+
+  settingsChange(setting) {
+    const hasOwn = Object.prototype.hasOwnProperty;
+
+    if (hasOwn.call(this.settings, setting)) {
+      this.settings[setting] = !this.settings[setting];
+      this.displaySettings(setting, this.settings[setting]);
+
+      console.log( '#model, @set setting ', setting, this.settings[setting] );
+    }
+  }
+
+  levelChange(level){
+    console.log( '@levelChange : ', level );
+  }
+
+  pageChange(page){
+    console.log( '@pageChange : ', page );
   }
 
   async updateWords() {
