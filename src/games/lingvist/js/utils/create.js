2202088e00d86e4445d68e9cda616265a823f87e
Igor <igorko.by@gmail.com>
2020-07-01 18:57:56 +0300

    feat: add util for creating DOM elements

---
+++ src/games/lingvist/js/utils/create.js
@@ -0,0 +1,31 @@
+export default function create(elem, classNames, child, parent, ...dataAttr) {
+  const element = document.createElement(elem);
+
+  if (classNames) element.classList.add(...classNames.split(' '));
+
+  if (child && Array.isArray(child)) {
+    child.forEach((childElement) => childElement && element.appendChild(childElement));
+  } else if (child && typeof child === 'object') {
+    element.appendChild(child);
+  } else if (child && typeof child === 'string') {
+    element.innerHTML = child;
+  }
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  if (dataAttr.length) {
+    dataAttr.forEach(([attrName, attrValue]) => {
+      if (attrValue === '') {
+        element.setAttribute(attrName, '');
+      }
+      if (attrName.match(/action|autofocus|target|href|type|src|alt|value|id|placeholder|autocorrect|autocomplete|spellcheck/)) {
+        element.setAttribute(attrName, attrValue);
+      } else {
+        element.dataset[attrName] = attrValue;
+      }
+    });
+  }
+  return element;
+}
