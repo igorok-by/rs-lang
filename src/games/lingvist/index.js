959b527344dbda35aaf9dcfb0b1e48dd3749ab41
Igor <igorko.by@gmail.com>
2020-07-02 11:52:44 +0300

    feat: add inserting input in card body

--- src/games/lingvist/index.js
+++ src/games/lingvist/index.js
@@ -2,16 +2,18 @@ import markup from './lingvist.html';
 import './styles.scss';
 import View from '../../services/view';
 import MainModel from '../../services/model';
+import Input from './js/Input';
 
 class Lingvist extends View {
   constructor() {
     super();
     this.hash = 'lingvist';
     this.mainModel = new MainModel();
-    this.scopeOfWords = [];
+    this.dataOfWords = [];
     this.audioBtn = this.createElement('button', 'card__audio');
     this.checkBtn = this.createElement('button', 'card__btn');
     this.lookBtn = this.createElement('button', 'card__btn');
+    this.answer = this.createElement('input', 'input__answer');
     this.form = '';
     this.header = '';
     this.body = '';
@@ -20,12 +22,13 @@ class Lingvist extends View {
 
   async getWords() {
     const words = await this.mainModel.getWords({ group: 0, page: 2 });
+    console.log(words);
     return words;
   }
 
   insertAudioBtn() {
-    const imageSVG = '<svg><use xlink:href="./img/sprite.svg#speaker"></use></svg>';
-    this.audioBtn.insertAdjacentHTML('beforeend', imageSVG);
+    const iconAudio = '<svg><use xlink:href="./img/sprite.svg#speaker"></use></svg>';
+    this.audioBtn.innerHTML = iconAudio;
     this.audioBtn.type = 'button';
 
     this.header = this.getElement('.card__header');
@@ -33,15 +36,24 @@ class Lingvist extends View {
   }
 
   insertControlBtns() {
-    this.checkBtn.insertAdjacentText('beforeend', 'Проверить');
+    this.checkBtn.innerHTML = 'Проверить';
     this.checkBtn.type = 'submit';
 
     this.lookBtn.type = 'button';
     this.lookBtn.classList.add('card__btn--light');
-    this.lookBtn.insertAdjacentText('beforeend', 'Показать ответ');
+    this.lookBtn.innerHTML = 'Показать перевод';
 
     this.footer = this.getElement('.card__footer');
     this.footer.append(this.lookBtn, this.checkBtn);
+  }
+
+  async insertLearning() {
+    const words = await this.getWords();
+    const input = new Input(words[0].word);
+
+    this.body.innerHTML = '';
+    // console.log(word);
+    this.body.append(input.createTemplate());
   }
 
   insertElementsAfterRender() {
@@ -55,6 +67,8 @@ class Lingvist extends View {
     const templateHTML = this.render(markup, { title: 'Lingvist' });
     show(templateHTML);
     this.insertElementsAfterRender();
+    this.insertLearning();
+    // console.log(this.form);
   }
 }
 
