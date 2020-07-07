4bd033a77451500ec6f3ca181bd98bfb0f9c2f3f
flatronsamatron <flatronsamatron@gmail.com>
2020-07-07 20:42:46 +0300

    speakit sprint

--- rs_lang-speakit/src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/main.scss';
-import NavigationPanel from './services/aside-panel';
-import Router from './services/route';
-
-(async () => {
-  NavigationPanel.init();
-  Router.init();
-})();
---
+++ speakIt/js/itemImage.js
@@ -0,0 +1,40 @@
+// import blank from '../img/speakit/blank.jpg'
+export default class ItemImage{
+    constructor(){
+        this.img = 'img/speakIt/blank.jpg';
+        this.description = '';
+        this.pHTML = null;
+        this.imgHTML = null;
+    }
+    renderHtmlElement(parent, tag, className, id, innerHtml, src) {
+        const element = document.createElement(tag);    
+        if (id) {
+          element.id = id;
+        }
+        if (innerHtml) {
+          element.innerText = innerHtml;
+        }
+        if(className) {
+           element.className = className; 
+        }
+        
+        if(src) {
+            element.src = src
+        }
+        parent.appendChild(element);    
+        return element;
+    }
+
+    render() {
+        const wrapperHTML = document.querySelector('.speak__main-img') 
+        this.imgHTML = this.renderHtmlElement(wrapperHTML, 'img', 'speak__img', null, null, this.img );
+        this.pHTML  = this.renderHtmlElement(wrapperHTML, 'p', 'speak__translate', null, this.description);
+    }
+
+    updateState(image, word) {
+        this.image = image;
+        this.description = word;
+        this.imgHTML.src = image;
+        this.pHTML.innerText = word;
+    }
+}
