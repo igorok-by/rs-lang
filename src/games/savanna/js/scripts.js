2aa7d07b26bb108fd2f2798f473eab22f993099e
Karpuk Konstantin <karpukkonstantin@gmail.com>
2020-07-15 19:21:42 +0300

    feat: fix game

--- src/games/savanna/js/scripts.js
+++ src/games/savanna/js/scripts.js
@@ -1,44 +1,18 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable import/newline-after-import */
-/* eslint-disable space-before-blocks */
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable eol-last */
-/* eslint-disable linebreak-style */
-/* eslint-disable semi */
-/* eslint-disable no-undef */
-/* eslint-disable prefer-arrow-savannaback */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-multi-spaces */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable comma-spacing */
-/* eslint-disable keyword-spacing */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable eqeqeq */
-/* eslint-disable quotes */
-/* eslint-disable arrow-spacing */
-/* eslint-disable no-param-reassign */
-/* eslint-disable space-infix-ops */
-/* eslint-disable prefer-const */
-/* eslint-disable func-names */
-/* eslint-disable prefer-template */
-/* eslint-disable no-unused-vars */
-/* eslint-disable comma-dangle */
-/* eslint-disable arrow-parens */
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
 import Model from '../../../services/model';
 const model = new Model();
 const errorAudio = new Audio('./img/savanna/error.mp3')
 const correctAudio = new Audio('./img/savanna/correct.mp3')
 
-export default function start() {
+export default async function start() {
     const background = document.querySelector('.savanna_game-wrapper');
     background.style.backgroundPositionY = '100%';
+    background.style.display = 'flex';
+    document.querySelector('.savanna__question-word-container').style.top = 0;
+    const heartContainer = document.querySelector('.savanna__lives-container');
+    
     heartConstructor();
-
+    
     function heartConstructor() {
-        const heartContainer = document.querySelector('.savanna__lives-container');
         const heart = '<div class="savanna__lives-item"></div>';
         for (let i = 0; i < 5; i++) {
             heartContainer.innerHTML += heart;
@@ -46,35 +20,36 @@ export default function start() {
     }
 
     document.querySelector('.savanna__new').onclick = () => {
-        document.querySelector('.savanna__result').style.display = 'none'
-        document.querySelector('.savanna_game-wrapper').style.display = 'flex'
-        document.querySelector('.savanna__cor_num').innerHTML = 0
-        document.querySelector('.savanna__er_num').innerHTML = 0
-        document.querySelectorAll('.savanna__res_word').forEach((el)=>{el.remove()})
-        start();
+        document.querySelector('.savanna__result').style.display = 'none';
+        document.querySelector('.savanna_game-wrapper').style.display = 'flex';
+        document.querySelector('.savanna__cor_num').innerHTML = 0;
+        document.querySelector('.savanna__er_num').innerHTML = 0;
+        heartContainer.innerHTML = '';
+        document.querySelectorAll('.savanna__res_word').forEach((el)=> { 
+            el.remove();
+        });
+        start(true);
     }
     
     let page = Math.floor(Math.random() * (5 - 0) + 0);
     let group = Math.floor(Math.random() * (29 - 0) + 0);
-    getWord(page, group);
+    await getWord(page, group);
 
     async function getWord(page, group) {  
         let fakeGroup = group >= 29 ? group-1 : group+1;
     
         const fakeWords = await model.getWords(page,fakeGroup);
         const trueWords = await model.getWords(page,group);
-    
-        const wordsInfo = {
+
+        check({
             trueWord: trueWords.map(el => el.word),
             fakeWord: fakeWords.map(el => el.word),
             audio: trueWords.map(el => el.audio),
             img: trueWords.map(el => el.image)
-        }
-        check(wordsInfo);
+        });
     }
 
-    function check(wordsInfo) {
-        const { trueWord, fakeWord, audio, img } = wordsInfo;
+    function check({ trueWord, fakeWord, audio, img }) {
         let arrSort = fakeWord.sort(() => Math.random() - 0.5);
     
         let arrWords = [trueWord[trueWord.length-1]];
@@ -82,7 +57,7 @@ export default function start() {
         for(let i = 0; i < 4; i++){
             arrWords.push(arrSort[i])
         }
-    
+        
         const rightWord = {
             eng: trueWord[trueWord.length-1],
             audio: audio[trueWord.length-1],
@@ -91,19 +66,27 @@ export default function start() {
     
         trueWord.splice(trueWord.length-1,1);
     
-        const allTrueWord = {
-            trueWord: trueWord,
-            fakeWord: fakeWord,
-            audio: audio,
-            img: img
-        }
-    
-        translateWord(arrWords, rightWord, allTrueWord);
+        translateWord(arrWords, rightWord, {
+            trueWord,
+            fakeWord,
+            audio,
+            img
+        });
         function translateWord(arrWords, rightWord, allTrueWord){
-            console.log('translate');
             const { eng, audio } = rightWord;
+            function questionWordTransition(transition) {
+                const questionWordContainer = document.querySelector('.savanna__question-word-container');
+        
+                if (!transition) {
+                    questionWordContainer.style = 'transition: top .5s linear; opacity: 0; top: 0;';
+                } else {
+                    questionWordContainer.style = 'transition: top 5s linear; opacity: 1; top: 50%;';
+                }
+            }
+            questionWordTransition(false);
             let urlAud = 'https://raw.githubusercontent.com/flatronsamatron/rslang-data/master/data/'
             let aud = new Audio(urlAud + audio.slice(6))
+            let p = null;
             const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200422T141128Z.dd52065382ad8a96.75a601c4137245008380fc26f2e31d94eaa0a27c&text= ${arrWords} &lang=en-ru`;
             fetch(url)
             .then(res => res.json())
@@ -111,51 +94,35 @@ export default function start() {
                 let words = data.text[0].trim().split(',');
                 let correctWord = words[0]
                 words.sort(() => Math.random() - 0.5);
-        
-                const questionWord = document.querySelector('.savanna__question-word');
-                
                 const trainWords = document.querySelector('.savanna__train-words');
-                
+                trainWords.innerHTML = '';
+                const heartContainer = document.querySelector('.savanna__lives-container');
+                const questionWord = document.querySelector('.savanna__question-word');
+                questionWord.innerHTML = eng;
+                words.forEach((el) => {
+                    p = document.createElement('p');
+                    p.innerHTML = el;
+                    p.className = 'savanna__train-word'
+                    trainWords.append(p);
+                    p.addEventListener('click', (e) => correctCheck(e.target, e.target.innerHTML));
+                });
+                questionWordTransition(true);        
+        
                 function correctCheck(target,text){
-                    if(text == corectWord){
+                    if(text == correctWord){
                         correct(target,text)
                     } else {
-                        error(corectWord)
+                        error(correctWord)
                     }
                 }
-        
-                function questionWordTransition(transition = fasle) {
-                    const questionWordContainer = document.querySelector('.savanna__question-word-container');
-                    if (!transition) {
-                        questionWordContainer.style.transition = '';
-                        questionWordContainer.style.transform = '';
-                    } else {
-                        questionWordContainer.style.transition = 'transition: all 5s linear';
-                        questionWordContainer.style.transform = 'translateY(50%)';
-                    }
-                }
-                round();
-                function round() {
-                    trainWords.innerHTML = '';
-                    const heartContainer = document.querySelector('.savanna__lives-container');
-                    allTrueWord.trueWord.length !==0 ? check(allTrueWord) : resultGame();
-                    heartContainer.children.length > 0 ? check(allTrueWord) : resultGame();
-                    questionWord.innerHTML = correctWord;
-                    words.forEach((el) => {
-                        let p = document.createElement('p');
-                        p.innerHTML = el;
-                        p.className = 'savanna__train-word'
-                        trainWords.append(p);
-                        p.addEventListener('click', (e) => correctCheck(e.target, e.target.innerHTML));
-                        /* p.onclick = (e) => {
-                            correctCheck(e.target, e.target.innerText);
-                        } */
-                    });
-                    questionWordTransition(true);
-                }
-        
+                const questionWordContainer = document.querySelector('.savanna__question-word-container');
+                questionWordContainer.addEventListener('transitionend', (e) => {
+                    const currentTopPosition = e.target.offsetTop;
+                    if (currentTopPosition !== 0) error(correctWord);
+                }, { once: true });
+                
                 function correct(target,text){
-                    
+                    questionWordTransition(false);
                     document.querySelectorAll('.savanna__train-word').forEach(el => {
                         el.style.pointerEvents = 'none'
                     })
@@ -163,9 +130,15 @@ export default function start() {
                     target.style.background = '#00c49d';
                     correctAudio.play();
                     bgTop();
+                    setTimeout(() => {
+                        allTrueWord.trueWord.length !==0 ? check(allTrueWord) : resultGame();
+                        if (heartContainer.children.length === 0) {
+                            resultGame();
+                        }
+                    }, 1000);
                     const p = document.createElement('p');
                     const image = document.createElement('img');
-                    image.src = './img/sprint/speaker.png'
+                    image.src = './img/sprint/speaker.png';
                     image.onclick = () => {
                         aud.play();
                     }
@@ -176,10 +149,13 @@ export default function start() {
                     const corChild =  document.querySelector('.savanna__correct').childNodes.length - 3
                     document.querySelector('.savanna__cor_num').innerHTML = corChild;
                 }
-        
+                
                 function error(text){
+                    questionWordTransition(false);
                     const heartItems = [...document.querySelectorAll('.savanna__lives-item')];
-                    heartItems[0].remove();
+                    const heartItem = document.querySelector('.savanna__lives-item');
+                    if (heartItems.length === 0) return false;
+                    heartItem.remove();
                     document.querySelectorAll('.savanna__word').forEach(el => {
                         el.style.pointerEvents = 'none';
                         if(el.innerHTML !== text){
@@ -188,10 +164,24 @@ export default function start() {
                     })
         
                     errorAudio.play();
+                    setTimeout(() => {
+                        allTrueWord.trueWord.length !==0 ? check(allTrueWord) : resultGame();
+                        if (heartContainer.children.length === 0) {
+                            resultGame();
+                        } 
+                    }, 1000);
+                    const p = document.createElement('p');
+                    const image = document.createElement('img');
+                    image.src = './img/sprint/speaker.png';
+                    image.onclick = () => {
+                        aud.play();
+                    }
+                    p.className = 'savanna__res_word'
+                    p.innerHTML = eng + ' - ' + text;
+                    p.append(image);
                     document.querySelector('.savanna__error').append(p);
                     const erChild =  document.querySelector('.savanna__error').childNodes.length - 3;
                     document.querySelector('.savanna__er_num').innerHTML = erChild;
-                    round();
                 }
         
                 function bgTop() {
@@ -207,16 +197,6 @@ export default function start() {
                 }
         
             });
-        }
-    
+        }    
     }
-
-
 }
-
-
-
-
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -17,19 +17,30 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: '/node_modules/',
-      }, {
+      },
+      {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         loader: 'file-loader',
         options: {
           name: './img/[name].[ext]',
         },
-      }, {
+      },
+      {
+        test: /\.(ttf|otf|webp|eot|woff|woff2)$/i, //
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts',
+        },
+      },
+      {
         test: /\.mp3$/,
         loader: 'file-loader',
         options: {
           name: './audio/[name].[ext]',
         },
-      }, {
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
@@ -40,9 +51,12 @@ module.exports = {
           }, {
             loader: 'postcss-loader',
             options: { sourceMap: true, config: { path: 'src/config/postcss.config.js' } },
+            loader : 'postcss-loader',
+            options : {sourceMap : true, config : {path : './src/config/postcss.config.js'}},
           },
         ],
-      }, {
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader',
@@ -53,6 +67,10 @@ module.exports = {
           }, {
             loader: 'postcss-loader',
             options: { sourceMap: true, config: { path: 'src/config/postcss.config.js' } },
+
+            loader  : 'postcss-loader',
+            options : {sourceMap : true, config : {path : './src/config/postcss.config.js'}},
+
           }, {
             loader: 'sass-loader',
             options: { sourceMap: true },
@@ -71,15 +89,15 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.css',
-    } ),
-    new CopyWebpackPlugin( [
-      { from: './src/assets/img/', to : './img/' },
-      { from: './src/games/english-puzle/assets/img', to: './img/english-puzle' },
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/assets/img/', to: './img/' },
+      { from: './src/games/english-puzzle/assets/img', to: './img/english-puzzle' },
       { from: './src/games/speakIt/assets/img', to: './img/speakIt' },
       { from: './src/games/audio-call/assets/img', to: './img/audio-call' },
       { from: './src/games/sprint/assets/img', to: './img/sprint' },
       { from: './src/games/savanna/assets/img', to: './img/savanna' }
-    ] ),
+    ]),
   ],
   resolve: {
     extensions: ['.js'],
