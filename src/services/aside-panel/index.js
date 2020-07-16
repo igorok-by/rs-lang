1a93aeb49544797ec9e1ff9ac655ae491b616d5d
rgovin <rgovin@allvision.by>
2020-07-16 04:17:38 +0300

    fix: navigate menu interactive

--- src/services/aside-panel/index.js
+++ src/services/aside-panel/index.js
@@ -12,10 +12,25 @@ class NavigationPanel {
     this.asideElements.insertAdjacentHTML('beforeend', itemHtml);
   }
 
+  bindItemClick() {
+    this.asideElements.addEventListener( 'click', ({target, currentTarget}) => {
+      const elements = Array.from( currentTarget.children );
+      const li = target.closest( 'li' );
+
+      elements.forEach( (el) => el.classList.remove( 'item__active' ) );
+
+      li.classList.add( 'item__active' );
+
+      // console.log(elements.indexOf(target))
+    } );
+  }
+
   init() {
     this.view.showIn(this.asidePanel, Markup);
 
     this.asideElements = this.view.getElement('.app-panel__elements');
+
+    this.bindItemClick();
   }
 }
 
